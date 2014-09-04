014c795c5bcbed68519e33868fa83607d756ccf3
supersherlock <diofel.pallega@gmail.com>
2014-09-04 20:17:15 +0800

    added

---
+++ exercise/javascript/globs.js
@@ -0,0 +1,33 @@
+//define functions and global variables here...
+var siteloc = "http://localhost/exercise";
+var scriptloc = "/scripts/"
+
+ 
+function square()
+{
+  $.ajax({
+      url: siteloc + scriptloc + "solvesquare.py",
+      data: {value:$("#value").val(),
+             },
+      success: function (res) { 
+                  $("p").append(res);
+                 
+  
+       } 
+    });
+}
+
+function cube()
+{
+  $.ajax({
+      url: siteloc + scriptloc + "solvecube.py",
+      data: {value:$("#value").val(),
+             }, 
+      success: function (res) {
+                  $("p").append(res);
+             
+
+       } 
+    });
+}
+  
